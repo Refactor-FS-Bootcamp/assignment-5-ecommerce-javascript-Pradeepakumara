1477fed5bc42a2fb45bd62a7cea5ac1fd07c1367
Pradeepakumara K R <pradeepkumarkaggundi@gmail.com>
2023-01-02 01:53:12 -1200

    Basic E-Commerce Website

---
+++ Assignment 5/dummy.js
@@ -0,0 +1,275 @@
+const sparksItems = [
+    // Item - 1
+    {
+        image : "https://m.media-amazon.com/images/W/WEBP_402378-T1/images/I/51g6S2XoAcL._UY500_.jpg",
+        title : "Sparks",
+        name : "Mens Sm-323 Sneaker",
+        ratings : '45,000',
+        offerdPrice : 692,
+        actualPrice : 899,
+        offer : "23% off",
+        delivery : "Free Delivery by Sparks" 
+    },
+
+    // Item - 2
+    {
+        image : "https://m.media-amazon.com/images/W/WEBP_402378-T1/images/I/51aeH0deKhL._UX500_.jpg",
+        title : "Sparks",
+        name : "Mens Sm 651 Industrial Shoe",
+        ratings : '6,943',
+        offerdPrice : 807,
+        actualPrice : '1,049',
+        offer : "23% off",
+        delivery : "Free Delivery by Sparks" 
+    },
+
+    // Item - 3
+    {
+        image : "https://m.media-amazon.com/images/W/WEBP_402378-T1/images/I/61wZ0-VlQwL._UY500_.jpg",
+        title : "Sparks",
+        name : "Mens Sx0500g Running Shoe",
+        ratings : '4,864',
+        offerdPrice : 769,
+        actualPrice : '1,049',
+        offer : "27% off",
+        delivery : "Free Delivery by Sparks" 
+    },
+
+    // Item - 4
+    {
+        image : "https://m.media-amazon.com/images/W/WEBP_402378-T1/images/I/811Xdo3kwvL._UX575_.jpg",
+        title : "Sparks",
+        name : "Mens Sm-728 Sneaker",
+        ratings : '127',
+        offerdPrice : '1,058',
+        actualPrice : '1,299',
+        offer : "19% off",
+        delivery : "Free Delivery by Sparks" 
+    },
+
+    // Item - 5
+    {
+        image : "https://m.media-amazon.com/images/W/WEBP_402378-T1/images/I/81+Q8+bNsCL._UX695_.jpg",
+        title : "Sparks",
+        name : "Men Sneakers",
+        ratings : '8,853',
+        offerdPrice : '882',
+        actualPrice : '1,199',
+        offer : "26% off",
+        delivery : "Free Delivery by Sparks" 
+    },
+
+    // Item - 6
+    {
+        image : "https://m.media-amazon.com/images/W/WEBP_402378-T1/images/I/81-TxeADroL._UX500_.jpg",
+        title : "Sparks",
+        name : "Mens Sx0379g Running Shoes",
+        ratings : '5,230',
+        offerdPrice : '1,199',
+        actualPrice : '1,499',
+        offer : "20% off",
+        delivery : "Free Delivery by Sparks" 
+    },
+
+    // Item - 7
+    {
+        image : "https://m.media-amazon.com/images/W/WEBP_402378-T1/images/I/61L4ZMMwgCL._UY500_.jpg",
+        title : "Sparks",
+        name : "Men's Canvas Sneakers",
+        ratings : '19,704',
+        offerdPrice : '846',
+        actualPrice : '949',
+        offer : "11% off",
+        delivery : "Free Delivery by Sparks" 
+    },
+
+    // Item - 8
+    {
+        image : "https://m.media-amazon.com/images/W/WEBP_402378-T1/images/I/81Ud8DjRbXL._UX500_.jpg",
+        title : "Sparks",
+        name : "Mens Sx0663g Running Shoes",
+        ratings : '3,610',
+        offerdPrice : '769',
+        actualPrice : '999',
+        offer : "23% off",
+        delivery : "Free Delivery by Sparks" 
+    },
+
+    // Item - 9
+    {
+        image : "https://m.media-amazon.com/images/W/WEBP_402378-T1/images/I/81NfRt2RF3L._UX575_.jpg",
+        title : "Sparks",
+        name : "Men's Running Shoes",
+        ratings : '13,958',
+        offerdPrice : '1,147',
+        actualPrice : '1,349',
+        offer : "15% off",
+        delivery : "Free Delivery by Sparks" 
+    },
+
+    // Item - 10
+    {
+        image : "https://m.media-amazon.com/images/W/WEBP_402378-T1/images/I/71nQIde8HCS._UY500_.jpg",
+        title : "Sparks",
+        name : "Trainers for Men for Gyming, Cross",
+        ratings : '572',
+        offerdPrice : '1,399',
+        actualPrice : '2,999',
+        offer : "53% off",
+        delivery : "Free Delivery by Sparks" 
+    },
+
+    // Item - 11
+    {
+        image : "https://m.media-amazon.com/images/W/WEBP_402378-T1/images/I/61rOuFqRG6L._UY500_.jpg",
+        title : "Sparks",
+        name : "Men's Rider-01 Men Shoes for Running with Breathable",
+        ratings : '6,947',
+        offerdPrice : '1,099',
+        actualPrice : '1,999',
+        offer : "45% off",
+        delivery : "Free Delivery by Sparks" 
+    },
+
+    // Item - 12
+    {
+        image : "https://m.media-amazon.com/images/W/WEBP_402378-T1/images/I/71JxwvpS7iL._UX500_.jpg",
+        title : "Sparks",
+        name : "Mens Sx0677g Running Shoe",
+        ratings : '3,315',
+        offerdPrice : '923',
+        actualPrice : '1,249',
+        offer : "26% off",
+        delivery : "Free Delivery by Sparks" 
+    },
+
+    // Item - 13
+    {
+        image : "https://m.media-amazon.com/images/W/WEBP_402378-T1/images/I/718SPSP6n5L._UX500_.jpg",
+        title : "Sparks",
+        name : "Men's Running Shoes",
+        ratings : '7,112',
+        offerdPrice : '1,192',
+        actualPrice : '1,499',
+        offer : "20% off",
+        delivery : "Free Delivery by Sparks" 
+    },
+
+    // Item - 14
+    {
+        image : "https://m.media-amazon.com/images/W/WEBP_402378-T1/images/I/713w+Um1HzL._UX500_.jpg",
+        title : "Sparks",
+        name : "Men's Sc0214g Loafers",
+        ratings : '12,127',
+        offerdPrice : '699',
+        actualPrice : '899',
+        offer : "22% off",
+        delivery : "Free Delivery by Sparks" 
+    },
+
+    // Item - 15
+    {
+        image : "https://m.media-amazon.com/images/W/WEBP_402378-T1/images/I/81yMLnn-f0L._UX575_.jpg",
+        title : "Sparks",
+        name : "Mens Sc0732g Sneaker",
+        ratings : '46',
+        offerdPrice : '967',
+        actualPrice : '1,099',
+        offer : "12% off",
+        delivery : "Free Delivery by Sparks" 
+    },
+
+    // Item - 16
+    {
+        image : "https://m.media-amazon.com/images/W/WEBP_402378-T1/images/I/71zFljxHt-L._UX500_.jpg",
+        title : "Sparks",
+        name : "Mens Sm-375 Sneaker",
+        ratings : '114',
+        offerdPrice : '846',
+        actualPrice : '1,099',
+        offer : "23% off",
+        delivery : "Free Delivery by Sparks" 
+    }
+
+
+];
+
+const container = document.querySelector('.item-container');
+
+// container.innerHTML = `<article class="sparks-item">
+// <img src=${sparksItems[0].image} alt="item1">
+// <div class="color-circle">
+//     <div class="wh"></div>
+//     <div class="bl"></div>
+//     <div class="gr"></div>
+//     <div class="re"></div>
+//     <div class="ye"></div>
+//     <div class="or"></div>
+// </div>
+// <h2>${sparksItems[0].title}</h2>
+// <p class="main-p1">${sparksItems[0].name}</p>
+// <div class="start-rating">
+//     <i class="fa-solid fa-star"></i>
+//     <i class="fa-solid fa-star"></i>
+//     <i class="fa-solid fa-star"></i>
+//     <i class="fa-solid fa-star"></i>
+//     <i class="fa-regular fa-star"></i>
+//     <span><i class="fa-solid fa-chevron-down"></i>${sparksItems[0].ratings}</span>
+// </div>
+// <div class="price">
+//     <p class="p1"><sup>&#8377;</sup>${sparksItems[0].offerdPrice}</p>
+//     <p class="p2">&#8377;${sparksItems[0].actualPrice}</p>
+//     <p class="p3">(${sparksItems[0].offer})</p>
+// </div>
+// <p class="main-p2"> <span>&#10003;</span>prime</p>
+// <p class="main-p3">${sparksItems[0].delivery}</p>
+// </article>`;
+
+// ForEach
+sparksItems.forEach(function(item){
+    container.innerHTML += `<article class="sparks-item">
+<img src=${item.image} alt="item1">
+<div class="color-circle">
+    <div class="wh"></div>
+    <div class="bl"></div>
+    <div class="gr"></div>
+    <div class="re"></div>
+    <div class="ye"></div>
+    <div class="or"></div>
+</div>
+<h2>${item.title}</h2>
+<p class="main-p1">${item.name}</p>
+<div class="start-rating">
+    <i class="fa-solid fa-star"></i>
+    <i class="fa-solid fa-star"></i>
+    <i class="fa-solid fa-star"></i>
+    <i class="fa-solid fa-star"></i>
+    <i class="fa-regular fa-star"></i>
+    <span><i class="fa-solid fa-chevron-down"></i>${item.ratings}</span>
+</div>
+<div class="price">
+    <p class="p1"><sup>&#8377;</sup>${item.offerdPrice}</p>
+    <p class="p2">&#8377;${item.actualPrice}</p>
+    <p class="p3">(${item.offer})</p>
+</div>
+<p class="main-p2"> <span>&#10003;</span>prime</p>
+<p class="main-p3">${item.delivery}</p>
+<button type="button" onclick="">Buy Now</button>
+<button type="button" onclick="">Add to cart</button>
+</article>`;
+});
+
+
+
+const btn = document.querySelector('.head-btn');
+const theme = document.querySelector('.item-section');
+const theme2 = document.querySelector('.header');
+const theme3 = document.querySelectorAll('.sparks-item');
+
+btn.addEventListener('click', function(){
+     theme.classList.toggle('back');
+     theme2.classList.toggle('head-back');
+     theme3.forEach(function(item){
+        item.classList.toggle('item-back');
+   })
+});
